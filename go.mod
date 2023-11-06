40324546bd86a830280b521b4cf77bfb30816f1e
Yoram de Langen <yoramdelangen@gmail.com>
2023-11-06 08:34:58 +0100

    changing to GIN
    Signed-off-by: Yoram de Langen <yoramdelangen@gmail.com>

--- go.mod
+++ go.mod
@@ -4,9 +4,19 @@ go 1.21.0
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/bytedance/sonic v1.10.2 // indirect
+	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
+	github.com/chenzhuoyu/iasm v0.9.1 // indirect
 	github.com/cloudflare/circl v1.3.3 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/gaukas/godicttls v0.0.4 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/gin-gonic/gin v1.9.1 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.15.5 // indirect
 	github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 // indirect
+	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/gofiber/fiber/v2 v2.50.0 // indirect
 	github.com/gofiber/template v1.8.2 // indirect
 	github.com/gofiber/template/html/v2 v2.0.5 // indirect
@@ -18,25 +28,39 @@ require (
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/imroc/req/v3 v3.42.1 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.16.7 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
+	github.com/labstack/echo/v4 v4.11.2 // indirect
+	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/onsi/ginkgo/v2 v2.12.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
 	github.com/quic-go/qpack v0.4.0 // indirect
 	github.com/quic-go/qtls-go1-20 v0.3.3 // indirect
 	github.com/quic-go/quic-go v0.38.1 // indirect
 	github.com/refraction-networking/utls v1.5.3 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/surrealdb/surrealdb.go v0.2.1 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/ugorji/go/codec v1.2.11 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.50.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/crypto v0.12.0 // indirect
+	golang.org/x/arch v0.5.0 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
 	golang.org/x/exp v0.0.0-20230817173708-d852ddb80c63 // indirect
 	golang.org/x/mod v0.12.0 // indirect
-	golang.org/x/net v0.14.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
 	golang.org/x/tools v0.12.1-0.20230815132531-74c255bcf846 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"crypto/md5"
 	"embed"
+	"encoding/hex"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"slices"
+	"strconv"
+	"strings"
 
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/template/html/v2"
+	"github.com/gin-gonic/gin"
 	"github.com/imroc/req/v3"
 	"github.com/surrealdb/surrealdb.go"
 	"github.com/yoramdelangen/iptv-web-app/query"
@@ -16,6 +21,9 @@ import (
 const (
 	HOST = "http://thu.watchbiptv.co:80"
 	PATH = "/player_api.php?username=%s&password=%s"
+
+	USERNAME = "4CNPVVkH7v"
+	PASSWORD = "946265932979"
 
 	STREAM      = "/%s/%s" // Username/Password
 	STREAM_LIVE = "/live/%s/%s"
@@ -38,16 +46,6 @@ var templates embed.FS
 type Movie map[string]interface{}
 type MovieCategory map[string]interface{}
 
-type AnyResponse[T any] interface {
-	Movie | MovieCategory
-}
-
-type SurrealResponse[T any] struct {
-	Result []T
-	Status bool
-	time   string
-}
-
 func surQuery[T any](query string, payload map[string]interface{}) T {
 	res, err := DB.Query(query, payload)
 	out, err := surrealdb.SmartUnmarshal[T](res, err)
@@ -59,47 +57,246 @@ func surQuery[T any](query string, payload map[string]interface{}) T {
 	return out
 }
 
-func Server() {
-	engine := html.NewFileSystem(http.FS(templates), ".html")
-	engine.Reload(true)
-	engine.Debug(true)
+func SimpleSelect[T any](thing string) T {
+	res, err := DB.Select(thing)
+	out, err := surrealdb.SmartUnmarshal[T](res, err)
 
-	app := fiber.New(fiber.Config{
-		Views:       engine,
-		ViewsLayout: "layouts/main",
-	})
-	app.Static("/", "./public")
+	if err != nil {
+		log.Println("Failed Unmarshal:", err)
+	}
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.Render("templates/main", fiber.Map{})
-	})
-
-	app.Get("/movies", func(c *fiber.Ctx) error {
-		qs := c.Context().QueryArgs()
-
-		sql := query.Movies
-		parameters := map[string]interface{}{}
-		if qs.Has("category") {
-			sql = query.MoviesByCategory
-			parameters["category_id"] = fmt.Sprintf("%d", qs.GetUintOrZero("category"))
-		}
-
-		movies := surQuery[[]Movie](sql, parameters)
-		categories := surQuery[[]MovieCategory](query.MovieCategories, map[string]interface{}{})
-
-		fmt.Printf("Category: %+v\n", fmt.Sprintf("%v", qs.GetUintOrZero("category")))
-
-		return c.Render("templates/movies/index", fiber.Map{
-			"ActiveCategory": fmt.Sprintf("%d", qs.GetUintOrZero("category")),
-			"Movies":         movies,
-			"Categories":     categories,
-		})
-	})
-
-	log.Fatal(app.Listen(":3000"))
+	return out
 }
 
+func Server() {
+	app := gin.Default()
+
+	// engine.AddFunc("image_url", func(url, title string) string {
+	// 	html := "<img lazy=\"loading\" src=\"%s\" alt=\"%s\" />"
+	// 	// if strings.HasPrefix(url, "data:") {
+	// 	// 	fmt.Println("image data url", url)
+	// 	// 	return url
+	// 	// }
+	// 	return template. fmt.Sprintf(html, url, title)
+	// 	// return fmt.Sprintf("/cache?image=%s", url)
+	// })
+
+	// app := fiber.New(fiber.Config{
+	// 	Views:       engine,
+	// 	ViewsLayout: "layouts/main",
+	// })
+	// app.Static("/", "./public")
+
+	// app.GET("/", func(c *gin.Context) {
+	// 	return c.Render("templates/index", fiber.Map{})
+	// })
+	//
+	// app.GET("/movies", func(c *gin.Context) error {
+	// 	qs := c.Context().QueryArgs()
+	//
+	// 	sql := query.Movies
+	// 	parameters := map[string]interface{}{}
+	// 	if qs.Has("category") {
+	// 		sql = query.MoviesByCategory
+	// 		parameters["category_id"] = fmt.Sprintf("%d", qs.GetUintOrZero("category"))
+	// 	}
+	//
+	// 	movies := surQuery[[]Movie](sql, parameters)
+	// 	categories := surQuery[[]MovieCategory](query.MovieCategories, map[string]interface{}{})
+	//
+	// 	fmt.Printf("Category: %+v\n", fmt.Sprintf("%v", qs.GetUintOrZero("category")))
+	//
+	// 	return c.Render("templates/movies/index", fiber.Map{
+	// 		"ActiveCategory": c.Query("category"),
+	// 		"Movies":         movies,
+	// 		"Categories":     categories,
+	// 	})
+	// })
+	//
+	// app.GET("/movies/:id", func(c *gin.Context) {
+	// 	thing := "movies:" + c.Params("id")
+	// 	movie := SimpleSelect[Movie](thing)
+	//
+	// 	if _, ok := movie["info"]; !ok {
+	// 		api := NewApi(DB)
+	//
+	// 		api.RunDetails(ACTION_MOVIE_INFO, c.Params("id"))
+	// 		movie = SimpleSelect[Movie](thing)
+	// 	}
+	//
+	// 	return c.Render("templates/movies/show", fiber.Map{
+	// 		"Movie": movie,
+	// 	})
+	// })
+
+	app.GET("/stream/:streamid/movies/:id", func(c *gin.Context) {
+		const oneMB = 1024 * 1024
+		const oneGB = 1024 * oneMB
+		const responseSize = 2 * oneGB
+
+		fmt.Println("")
+		fmt.Println("")
+
+		fmt.Printf("Params: %+v\n", c.Params)
+		fmt.Printf("Headers: %+v\n", c.Request.Header)
+
+		// streamId := c.ParamsInt("streamid")
+		id, err := strconv.Atoi(strings.Split(c.Param("id"), ".")[0])
+		if err != nil {
+			fmt.Println("Error parsing params", err)
+			return
+		}
+
+		fmt.Println("id", id)
+
+		host := "http://thu.watchbiptv.co:80"
+		creds := "4CNPVVkH7v/946265932979"
+		url := fmt.Sprintf("%s/movie/%s/%d.mkv", host, creds, id)
+
+		fmt.Println("URL to load", url)
+
+		r, _ := http.NewRequest(http.MethodGet, url, nil)
+
+		// add headers to request
+		for h, v := range c.Request.Header {
+			r.Header.Add(h, v[0])
+		}
+
+		client := http.Client{
+			CheckRedirect: func(req *http.Request, via []*http.Request) error {
+				fmt.Println("Redirected to:", req.URL)
+				fmt.Printf("Redirect headers: %+v\n", req.Header)
+				// http.ErrUseLastResponse
+				fmt.Println("Via:", via)
+				fmt.Println("")
+				return nil
+			},
+		}
+		resp, err := client.Do(r)
+		if err != nil {
+			fmt.Printf("Request error: %s", err)
+			return
+		}
+
+		allowHeaders := []string{
+			"Server",
+		}
+
+		fmt.Println("")
+		fmt.Println("SET HEADERS FROM REQUEST")
+		// copy all headers back to the response
+		for header := range resp.Header {
+			if slices.Contains(allowHeaders, header) {
+				continue
+			}
+			fmt.Println(header, resp.Header.Get(header))
+			c.Writer.Header().Add(header, resp.Header.Get(header))
+		}
+
+		defer resp.Body.Close()
+
+		fmt.Println("")
+
+		// bytesRead := 0
+		fmt.Println("ALREADY SET HEADERS", c.Writer.Header())
+
+		_, err = io.Copy(c.Writer, resp.Body)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to stream video"})
+			return
+		}
+
+		// buf := make([]byte, oneMB*4)
+		// c.Stream(func(w io.Writer) bool {
+		// 	select {
+		// 	case <-ctx.Done():
+		// 		fmt.Println("Request canceled")
+		// 		return false // Stop streaming
+		// 	default:
+		// 		n, err := resp.Body.Read(buf)
+		// 		bytesRead += n
+		// 		if err != nil {
+		// 			if err == io.EOF {
+		// 				fmt.Println("EOF")
+		// 				return false
+		// 			}
+		//
+		// 			fmt.Println("Error reading data from the external URL:", err)
+		// 			return false
+		// 		}
+		//
+		// 		// fmt.Println("Totally written: ", bytesRead)
+		// 		_, writeErr := c.Writer.Write(buf[:n])
+		// 		if writeErr != nil {
+		// 			fmt.Println("Error writing data:", writeErr)
+		// 			return false
+		// 		}
+		//
+		// 		return true
+		// 	}
+		// })
+	})
+
+	// app.GET("/cache", func(c *gin.Context) error {
+	// 	qs := c.Context().QueryArgs()
+	//
+	// 	cp, _ := os.Getwd()
+	// 	storagePath := filepath.Join(cp, "storage")
+	//
+	// 	// create storage folder
+	// 	if _, err := os.Stat(storagePath); errors.Is(err, os.ErrNotExist) {
+	// 		os.Mkdir(storagePath, 0755)
+	// 	}
+	//
+	// 	if qs.Has("image") {
+	// 		img := strings.TrimSpace(c.Query("image"))
+	//
+	// 		// fmt.Println("ext", path.Ext(img))
+	// 		imgHash := GetMD5Hash(img) + path.Ext(img)
+	// 		fileStorage := filepath.Join(storagePath, imgHash)
+	//
+	// 		if _, err := os.Stat(fileStorage); errors.Is(err, os.ErrNotExist) {
+	//
+	// 			if strings.HasPrefix(img, "data:") {
+	// 				fmt.Println("data image", img)
+	// 				return c.SendString(img)
+	// 			}
+	//
+	// 			// we should download it.
+	// 			resp, err := http.Get(img)
+	// 			if err != nil {
+	// 				fmt.Println("Error, err")
+	// 				return c.Status(404).SendString("Not found because of error: " + err.Error())
+	// 			}
+	//
+	// 			defer resp.Body.Close()
+	//
+	// 			out, err := os.Create(fileStorage)
+	// 			defer out.Close()
+	//
+	// 			_, err = io.Copy(out, resp.Body)
+	// 			if err != nil {
+	// 				fmt.Println("Error, err")
+	// 				return c.Status(404).SendString("Storing failed: " + err.Error())
+	// 			}
+	//
+	// 			return c.SendStream(out)
+	// 		}
+	//
+	// 		return c.SendFile(fileStorage, true)
+	// 	}
+	//
+	// 	return c.Status(404).SendString("Nothing to see")
+	//
+	// })
+
+	log.Fatal(app.Run("localhost:3000"))
+}
+
+// ====================================================
+
 type Response = []map[string]interface{}
+type ResponseDetail = map[string]interface{}
 
 func SyncXtreamApi() {
 	api := NewApi(DB)
@@ -122,8 +319,10 @@ const (
 	ACTION_NONE ActionType = iota
 	ACTION_MOVIE_CATEGORIES
 	ACTION_MOVIES
-	ACTION_TVSHOWS
+	ACTION_MOVIE_INFO
 	ACTION_TVSHOW_CATEGORIES
+	ACTION_TVSHOWS
+	ACTION_TVSHOW_INFO
 	ACTION_LIVESTREAMS
 	ACTION_LIVESTREAM_CATEGORIES
 )
@@ -150,10 +349,15 @@ func (x XtreamApi) RunAll() {
 	fmt.Println("Ran all actions")
 }
 
+func (x XtreamApi) url(action string) string {
+	path := fmt.Sprintf(PATH, USERNAME, PASSWORD)
+
+	return fmt.Sprintf("%s%s&action=%s", HOST, path, action)
+}
+
 func (x XtreamApi) RunSingle(_type ActionType) {
 	action := x.actions[_type]
-	path := fmt.Sprintf(PATH, "4CNPVVkH7v", "946265932979")
-	url := fmt.Sprintf("%s%s&action=%s", HOST, path, action.Action)
+	url := x.url(action.Action)
 
 	fmt.Println("Starting", action.Action)
 
@@ -209,23 +413,30 @@ func (x XtreamApi) CategoryStats() {
 		})
 
 		fmt.Println("Finished for table", action.Table)
+	}
+}
 
-		// ACTION_MOVIE_CATEGORIES: {
-		// 	Action:  "get_vod_categories",
-		// 	Table:   "movie_categories",
-		// 	IdField: "category_id",
-		// 	IdType:  ACTION_ID_TYPE_STRING,
-		// 	For:     ACTION_MOVIES,
-		// },
-		// ACTION_MOVIES: {
-		// 	Action:  "get_vod_streams",
-		// 	Table:   "movies",
-		// 	IdField: "stream_id",
-		// 	IdType:  ACTION_ID_TYPE_FLOAT,
-		// 	For:     ACTION_NONE,
-		// },
+func (x XtreamApi) RunDetails(_type ActionType, id string) {
+	action := x.actions[_type]
+	url := x.url(action.Action) + "&vod_id=" + id
+
+	resp, err := x.client.Get(url)
+	if err != nil {
+		log.Fatal(err)
 	}
 
+	res := &ResponseDetail{}
+	resp.Unmarshal(res)
+
+	thing := fmt.Sprintf("%s:%v", action.Table, id)
+	query := "UPDATE $what MERGE $data"
+	params := map[string]interface{}{
+		"what": thing,
+		"data": res,
+	}
+	if _, err = x.db.Query(query, params); err != nil {
+		fmt.Printf("Failed Info update [%s] %s, error %s\n", action.Table, thing, err)
+	}
 }
 
 func NewApi(db *surrealdb.DB) *XtreamApi {
@@ -247,6 +458,13 @@ func NewApi(db *surrealdb.DB) *XtreamApi {
 			IdType:  ACTION_ID_TYPE_FLOAT,
 			For:     ACTION_NONE,
 		},
+		ACTION_MOVIE_INFO: {
+			Action:  "get_vod_info",
+			Table:   "movies",
+			IdField: "vod_id",
+			IdType:  ACTION_ID_TYPE_FLOAT,
+			For:     ACTION_NONE,
+		},
 		ACTION_TVSHOW_CATEGORIES: {
 			Action:  "get_series_categories",
 			Table:   "tvshow_categories",
@@ -256,6 +474,13 @@ func NewApi(db *surrealdb.DB) *XtreamApi {
 		},
 		ACTION_TVSHOWS: {
 			Action:  "get_series",
+			Table:   "tvshows",
+			IdField: "series_id",
+			IdType:  ACTION_ID_TYPE_FLOAT,
+			For:     ACTION_NONE,
+		},
+		ACTION_TVSHOW_INFO: {
+			Action:  "get_serie_info",
 			Table:   "tvshows",
 			IdField: "series_id",
 			IdType:  ACTION_ID_TYPE_FLOAT,
@@ -302,4 +527,9 @@ func Db() *surrealdb.DB {
 	db.Use("iptv_ns", "iptv_db")
 
 	return db
+}
+
+func GetMD5Hash(text string) string {
+	hash := md5.Sum([]byte(text))
+	return hex.EncodeToString(hash[:])
 }
